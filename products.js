528543592f4c6426a5d29bdcfc70f267472cfb90
mr-soum3n <soumen.barai390+github@gmail.com>
2026-03-10 02:27:30 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,25 @@
+fetch("stickers.json")
+.then(response => response.json())
+.then(data => {
+
+const container = document.getElementById("sticker-container")
+
+data.forEach(sticker => {
+
+const card = document.createElement("div")
+card.className = "product-card"
+
+card.innerHTML = `
+<img src="${sticker.image}">
+<h3>${sticker.name}</h3>
+<p class="price">₹${sticker.price}</p>
+<button onclick="addToCart('${sticker.name}',${sticker.price})">
+Add to Cart
+</button>
+`
+
+container.appendChild(card)
+
+})
+
+})
