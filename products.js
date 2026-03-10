5f2112b24b8d51c987d184e7644260c4350c5b0e
mr-soum3n <soumen.barai390+github@gmail.com>
2026-03-11 00:40:29 +0530

    Update products.js

--- products.js
+++ products.js
@@ -25,3 +25,34 @@ container.appendChild(card)
 })
 
 })
+
+
+let visibleProducts = 8;
+
+function renderProducts(data){
+
+const container = document.getElementById("sticker-container");
+container.innerHTML="";
+
+data.slice(0,visibleProducts).forEach(sticker =>{
+
+const card=document.createElement("div");
+card.className="product-card";
+
+card.innerHTML=`
+<img src="${sticker.image}">
+<h3>${sticker.name}</h3>
+<p class="price">₹${sticker.price}</p>
+<button>Add to Cart</button>
+`;
+
+container.appendChild(card);
+
+});
+
+}
+
+document.getElementById("showMoreBtn").onclick=function(){
+visibleProducts += 4;
+loadProducts();
+};
